c96d025a69786c3d8d67f2e859b750030b7ffa3a
Collinsdeveloper <kipngetichcollins19@gmail.com>
2026-07-01 13:44:12 +0300

    Implement POST events endpoint

--- app.py
+++ app.py
@@ -7,3 +7,18 @@ def home():
 @app.route("/events", methods=["GET"])
 def get_events():
     return jsonify([event.to_dict() for event in events])
+
+@app.route("/events", methods=["POST"])
+def create_event():
+    data = request.get_json()
+
+    new_id = max([event.id for event in events], default=0) + 1
+
+    new_event = Event(
+        new_id,
+        data["title"]
+    )
+
+    events.append(new_event)
+
+    return jsonify(new_event.to_dict()), 201
